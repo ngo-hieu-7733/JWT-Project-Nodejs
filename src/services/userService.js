0ea936fcf0e5bd8743f9c57941893714f64af784
ngo-hieu-7733 <ngohieu815@gmail.com>
2025-11-03 18:12:42 +0700

    add login api

--- src/services/userService.js
+++ src/services/userService.js
@@ -21,6 +21,37 @@ const createUserService = async (name, email, password) => {
 	}
 };
 
+const loginService = async (email, password) => {
+	try {
+		// hash user's password
+		const user = await User.findOne({ email });
+
+		if (user) {
+			const res = await bcrypt.compare(password, user.password);
+			if (res) {
+				return {
+					code: 201,
+					message: 'dang nhap thanh cong',
+					token: 'access-token',
+				};
+			}
+			return {
+				code: 2,
+				message: 'email / password khong hop le',
+			};
+		} else {
+			return {
+				code: 1,
+				message: 'email / password khong hop le',
+			};
+		}
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
+
 module.exports = {
 	createUserService,
+	loginService,
 };
